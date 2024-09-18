5cc8590845a442138d5115165a6338a445d68f47
Justin Wamback <justin.wamback@mda.space>
2024-09-18 13:02:07 +0000

    CIP-2867 update driver code for logger changes

--- raw/ska-mid-cbf-agilex-vcc/driver_source_code/fpga_driver_common/driver/fpga_driver_base_pybind.cpp
+++ raw/ska-mid-cbf-agilex-vcc/driver_source_code/fpga_driver_common/driver/fpga_driver_base_pybind.cpp
@@ -6,6 +6,20 @@
 namespace py = pybind11;
 using namespace fpga_driver;
 
+class PyLogger : public Logger {
+public:
+    using Logger::Logger;
+
+    virtual void log_message(LogLevel severity, std::string msg) override {
+        PYBIND11_OVERRIDE(
+            void,        /* Return type */
+            Logger,      /* Parent class */
+            log_message, /* Name of function in C++ (must match Python name) */
+            severity,    /* Argument(s) */
+            msg
+        );
+    }
+};
 
 FPGA_DRIVER_PYBIND_MODULE_DECLARATION(fpga_driver_base, m) {
 
@@ -26,7 +40,7 @@ FPGA_DRIVER_PYBIND_MODULE_DECLARATION(fpga_driver_base, m) {
         .def_readwrite("depth", &RegisterSetInfo::depth)
         .def_readwrite("version", &RegisterSetInfo::version);
 
-    py::class_<Logger>(m, "Logger")
+    py::class_<Logger, PyLogger>(m, "Logger")
       .def(py::init<LogLevel>(), py::arg("severity") = LogLevel::Warning)
       .def("set_logging_level", &Logger::set_logging_level, py::arg("severity"))
       .def("get_logging_level", &Logger::get_logging_level)
@@ -36,5 +50,7 @@ FPGA_DRIVER_PYBIND_MODULE_DECLARATION(fpga_driver_base, m) {
       .def("info", &Logger::info, py::arg("msg"))
       .def("pass", &Logger::pass, py::arg("msg"))
       .def("debug", &Logger::debug, py::arg("msg"))
-      .def("trace", &Logger::trace, py::arg("msg"));
+      .def("trace", &Logger::trace, py::arg("msg"))
+      .def("log_message", &PyLogger::log_message, py::arg("severity"), py::arg("msg"));
+
 }
